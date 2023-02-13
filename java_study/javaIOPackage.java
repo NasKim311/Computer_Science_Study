cc48815de42fa991c95d23cf149ccf426fbb7141
Nas <kimna0311@naver.com>
2023-02-13 17:28:55 +0900

    java_IO_study_commit

--- java_study/javaIOPackage.java
+++
@@ -1,7 +0,0 @@
-package java_study;
-
-public class javaIOPackage {
-public static void main(String[] args) {
-}
-    
-}
---
+++ java_study/java_IO_study/src/java_IO_study/javaIOPackage.java
@@ -0,0 +1,7 @@
+package java_study.java_IO_study.src.java_IO_study;
+public class javaIOPackage {
+public static void main(String[] args) {
+        
+    } // main method
+   
+} // javaIOPackage class
